c4651b736f35f41930b93294dd4821596c43e1e9
Elizaveta Sh <shestakovaliza@gmail.com>
2020-10-05 11:23:56 +0700

    Style the selected hero

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -11,15 +11,16 @@ export class HeroesComponent implements OnInit {
 
   heroes = HEROES;
   selectedHero: Hero;
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
 
-
+  
   constructor() { }
 
 
   ngOnInit() {
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 
 }
